4519fd5c9179e55fe4f9df3d13bbc2478b8e75b3
Krasulenko <krasul@ukr.net>
2016-09-04 01:33:54 +0300

    Homework 3.4

---
+++ src/homework34/Main.java
@@ -0,0 +1,18 @@
+package homework34;
+
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Michael", 3000, 24, "Oracle", 4000, "USD");
+
+        System.out.println("Balance" + " " + user.getBalance());
+        user.paySalary();
+        System.out.println("Balance after sallary" + " " + user.getBalance());
+
+        System.out.println("Withdrawal" + " " + user.withdraw(1600));
+
+        System.out.println("Companyname length" + " " + user.companyNameLength());
+
+        System.out.println("Months of employment" + " " + user.monthIncreaser(6));
+    }
+}
