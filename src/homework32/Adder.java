c9ccd0d1521ae7a1dc1cd144aae1809a8c71cf03
Krasulenko <krasul@ukr.net>
2016-09-07 09:56:15 +0300

    Changes2 after comments in Homework 3.2

--- src/homework32/Adder.java
+++ src/homework32/Adder.java
@@ -2,9 +2,9 @@ package homework32;
 
 public class Adder extends Arithmetic{
     protected boolean check(Integer a, Integer b){
-        boolean check = false;
-        if (a>=b) check = true;
-        return check;
+        //boolean check = false;
+        //if (a>=b) check = true;
+        return a>=b;
     }
 
 }
